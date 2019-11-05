1f7741a5f266658e2a97b036d880c4f3e8cfede7
oalmaguer <almaguero95@gmail.com>
2019-11-04 20:42:02 -0500

    added search by title feature

--- script/script.js
+++ script/script.js
@@ -1,208 +1,336 @@
- var movie = $("#movieSearch").val();
-        var queryURL = "http://www.omdbapi.com/?apikey=dcedfc78&s=" +movie;
-       
+var movie = $("#movieSearch").val();
+var queryURL = "http://www.omdbapi.com/?apikey=dcedfc78&s=" + movie;
+
+$("#btnSearch").on("click", function() {
+  event.preventDefault();
+
+  var movie = $("#movieSearch :selected").val();
+  var queryURL = "http://www.omdbapi.com/?apikey=dcedfc78&t=" + movie;
+
+  var queryURLYear =
+    "https://api.themoviedb.org/3/discover/movie?api_key=3ce487e5f4b9c4f747c250ad1fb0e3b0&language=en-US&primary_release_year=" +
+    movie +
+    "";
+
+  var queryURLTrending =
+    "https://api.themoviedb.org/3/trending/all/week?api_key=3ce487e5f4b9c4f747c250ad1fb0e3b0";
+
+  //YEAR
+
+  $.ajax({
+    url: queryURLYear,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+
+    var arrayMovies = [];
+    var arrayOverview = [];
+    var arrayYear = [];
+    var arrayPoster = [];
+
+    for (let i = 0; i < 20; i++) {
+      let moviePopular = response.results[i].original_title;
+      let movieOverview = response.results[i].overview;
+      let moviePoster = response.results[i].poster_path;
+      let movieDate = response.results[i].release_date;
+
+      queryPoster = "http://image.tmdb.org/t/p/w185//" + moviePoster + "";
+
+      // // let movieActors = response.Actors;
+      // // let movieRatings = response.Ratings[2];
+      // // let moviePlot = response.Plot;
+      popular = moviePopular;
+      overview = movieOverview;
+      poster = queryPoster;
+      date = movieDate;
+
+      arrayMovies.push(popular);
+      arrayYear.push(date);
+      arrayOverview.push(overview);
+      arrayPoster.push(poster);
+      // ratings = JSON.stringify(movieRatings);
+      // plot = JSON.stringify(moviePlot);
+    }
+    console.log(arrayMovies);
+
+    $("tbody").empty();
+    $(".content").append("<table>");
+    $("tbody").append("<th>" + "Title" + "</th");
+    $("tbody").append("<th>" + "Year" + "</th");
+    $("tbody").append("<th>" + "Plot" + "</th");
+    $("tbody").addClass("thead-dark");
+
+    for (i = 0; i < arrayMovies.length; i++) {
+      $(".table-movie").append(
+        "<tr><td>" +
+          arrayMovies[i] +
+          "<img src=" +
+          arrayPoster[i] +
+          " class=" +
+          "imgClass" +
+          "></td><td>" +
+          arrayYear[i] +
+          "</td><td>" +
+          arrayOverview[i] +
+          "</td></tr>"
+      );
+    }
+  });
+});
+
+//TRENDING
+
+$("#btnTrend").on("click", function() {
+  event.preventDefault();
+
+  var queryURLTrending =
+    "https://api.themoviedb.org/3/trending/all/week?api_key=3ce487e5f4b9c4f747c250ad1fb0e3b0";
+
+  $.ajax({
+    url: queryURLTrending,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+
+    var arrayMovies = [];
+    var arrayOverview = [];
+    var arrayYear = [];
+    var arrayPoster = [];
+
+    for (let i = 0; i < 20; i++) {
+      var moviePopular = response.results[i].original_title;
+      var movieOverview = response.results[i].overview;
+      var moviePoster = response.results[i].poster_path;
+      var movieDate = response.results[i].release_date;
+
+      queryPoster = "http://image.tmdb.org/t/p/w185//" + moviePoster + "";
+
+      // // let movieActors = response.Actors;
+      // // let movieRatings = response.Ratings[2];
+      // // let moviePlot = response.Plot;
+      popular = moviePopular;
+      overview = movieOverview;
+      poster = queryPoster;
+      date = movieDate;
+
+      if (typeof response.results[i].original_title != "undefined") {
+        arrayMovies.push(popular);
+        arrayYear.push(date);
+        arrayOverview.push(overview);
+        arrayPoster.push(poster);
+      }
+    }
+    console.log(arrayMovies);
+
+    $("tbody").empty();
+    $(".content").append("<table>");
+    $("tbody").append("<th>" + "Title" + "</th");
+    $("tbody").append("<th>" + "Year" + "</th");
+    $("tbody").append("<th>" + "Plot" + "</th");
+    $("tbody").addClass("thead-dark");
+
+    for (i = 0; i < arrayMovies.length; i++) {
+      $(".table-movie").append(
+        "<tr><td>" +
+          arrayMovies[i] +
+          "<img src=" +
+          arrayPoster[i] +
+          " class=" +
+          "imgClass" +
+          "></td><td>" +
+          arrayYear[i] +
+          "</td><td>" +
+          arrayOverview[i] +
+          "</td></tr>"
+      );
+    }
+  });
+});
+
+//by genre
+
+$("#btnGenre").on("click", function() {
+  event.preventDefault();
+
+  var movie = $("#movieSearch :selected").val();
+  var genreSelected = $("#movieSearchGenre").val();
+  console.log(genreSelected);
+
+  var queryURLGenre =
+    "https://api.themoviedb.org/3/discover/movie?api_key=3ce487e5f4b9c4f747c250ad1fb0e3b0&language=en-US&with_genres=" +
+    genreSelected +
+    "&primary_release_year=" +
+    movie +
+    "";
+
+  $.ajax({
+    url: queryURLGenre,
+    method: "GET"
+  }).then(function(response) {
+    console.log(response);
+    var arrayMovies = [];
+    var arrayOverview = [];
+    var arrayYear = [];
+    var arrayPoster = [];
+    var arrayAverage = [];
+    var arrayLinks = [];
+
+    for (let i = 0; i < 20; i++) {
+      var moviePopular = response.results[i].original_title;
+      var movieOverview = response.results[i].overview;
+      var moviePoster = response.results[i].poster_path;
+      var movieDate = response.results[i].release_date;
+      var movieAverage = response.results[i].vote_average;
+
+      queryPoster = "http://image.tmdb.org/t/p/w185//" + moviePoster + "";
+
+      // // let movieActors = response.Actors;
+      // // let movieRatings = response.Ratings[2];
+      // // let moviePlot = response.Plot;
+      popular = moviePopular;
+      overview = movieOverview;
+      poster = queryPoster;
+      date = movieDate;
+      average = movieAverage;
+      var movieLinks = "https://cuevana2.io/pelicula/" + popular;
+      links = movieLinks.replace(/ +/g, "-");
+
+      if (typeof response.results[i].original_title != "undefined") {
+        arrayMovies.push(popular);
+        arrayYear.push(date);
+        arrayOverview.push(overview);
+        arrayPoster.push(poster);
+        arrayAverage.push(average);
+        arrayLinks.push(links);
+      }
+    }
+    console.log(arrayMovies);
+    console.log(arrayLinks);
+
+    $("tbody").empty();
+    $(".content").append("<table>");
+    $("tbody").append("<th>" + "Title" + "</th");
+    $("tbody").append("<th>" + "Year" + "</th");
+    $("tbody").append("<th>" + "Plot" + "</th");
+    $("tbody").append("<th>" + "Average" + "</th");
+    $("tbody").addClass("thead-dark");
+
+    for (i = 0; i < arrayMovies.length; i++) {
+      $(".table-movie").append(
+        "<tr><td>" +
+          arrayMovies[i] +
+          "<img src=" +
+          arrayPoster[i] +
+          " class=" +
+          "imgClass" +
+          "></td><td>" +
+          arrayYear[i] +
+          "</td><td>" +
+          arrayOverview[i] +
+          " " +
+          "<a href=" +
+          arrayLinks[i] +
+          ">" +
+          arrayMovies[i] +
+          "</a>" +
+          "</td><td>" +
+          arrayAverage[i] +
+          "</td></tr>"
+      );
+    }
+  });
+});
+
+$(".btnTitle").on("click", function() {
+  event.preventDefault();
+
+  var movieTitle = $("#movieSearchTitle").val().trim();
+  movieTitle = movieTitle.replace(/ +/g, "%20");
+  console.log(movieTitle);
 
 
+  var querySearchTitle =
+    "https://api.themoviedb.org/3/search/movie?api_key=3ce487e5f4b9c4f747c250ad1fb0e3b0&language=en-US&query="+movieTitle+"";
+
+  $.ajax({
+    url: querySearchTitle,
+    method: "GET"
+  }).then(function(responseTitle) {
+    
+    var arrayMovies = [];
+    var arrayOverview = [];
+    var arrayYear = [];
+    var arrayPoster = [];
+
+    for (var i = 0; i < 20; i++) {
+      var moviePopular = responseTitle.results[i].original_title;
+      console.log(moviePopular);
+      var movieOverview = responseTitle.results[i].overview;
+      var moviePoster = responseTitle.results[i].poster_path;
+      var movieDate = responseTitle.results[i].release_date;
+
+      queryPoster = "http://image.tmdb.org/t/p/w185//" + moviePoster + "";
+
+      // // let movieActors = response.Actors;
+      // // let movieRatings = response.Ratings[2];
+      // // let moviePlot = response.Plot;
+      
+      overview = movieOverview;
+      poster = queryPoster;
+      date = movieDate;
+
+      if (typeof moviePopular != "undefined") {
+        arrayMovies.push(moviePopular);
+        arrayYear.push(date);
+        arrayOverview.push(overview);
+        arrayPoster.push(poster);
         
-        $("#btnSearch").on("click", function(){
-            event.preventDefault();
+      }
+    }
+    
 
-             var movie = $("#movieSearch :selected").val();
-             var queryURL = "http://www.omdbapi.com/?apikey=dcedfc78&t=" +movie;
+    $("tbody").empty();
+    $(".content").append("<table>");
+    $("tbody").append("<th>" + "Title" + "</th");
+    $("tbody").append("<th>" + "Year" + "</th");
+    $("tbody").append("<th>" + "Plot" + "</th");
+    $("tbody").addClass("thead-dark");
 
-             var queryURLYear = "https://api.themoviedb.org/3/discover/movie?api_key=3ce487e5f4b9c4f747c250ad1fb0e3b0&language=en-US&primary_release_year="+movie+"";
+    for (i = 0; i < arrayMovies.length; i++) {
+      $(".table-movie").append(
+        "<tr><td>" +
+          arrayMovies[i] +
+          "<img src=" +
+          arrayPoster[i] +
+          " class=" +
+          "imgClass" +
+          "></td><td>" +
+          arrayYear[i] +
+          "</td><td>" +
+          arrayOverview[i] +
+          "</td></tr>"
+      );
+    }
+  });
 
-             var queryURLTrending = "https://api.themoviedb.org/3/trending/all/week?api_key=3ce487e5f4b9c4f747c250ad1fb0e3b0";
+});
 
-            //YEAR
-            
+//books
 
-        $.ajax ({
-            url: queryURLYear,
-            method: 'GET'
-        }).then(function(response) {
-            console.log(response);
+//      $("#btnGenre").on("click", function(){
+//         event.preventDefault();
 
+//         // var movie = $("#movieSearch :selected").val();
+//         // var genreSelected = $("#movieSearchGenre").val();
+//         // console.log(genreSelected);
 
-            var arrayMovies = [];
-            var arrayOverview = [];
-            var arrayYear = [];
-            var arrayPoster = [];
+//         var queryURLBook = 'http://openlibrary.org/search.json?title=the+lord+of+the+rings';
 
-            for (let i=0;i<20;i++){
-            let moviePopular = response.results[i].original_title;
-            let movieOverview = response.results[i].overview;
-            let moviePoster = response.results[i].poster_path;
-            let movieDate = response.results[i].release_date;
-            
-           
-            queryPoster = "http://image.tmdb.org/t/p/w185//"+moviePoster+"";
-            
-            // // let movieActors = response.Actors;
-            // // let movieRatings = response.Ratings[2];
-            // // let moviePlot = response.Plot;
-            popular = moviePopular;
-            overview = movieOverview;
-            poster = queryPoster;
-            date = movieDate;
+//     $.ajax ({
+//         url: queryURLBook,
+//         method: 'GET'
+//     }).then(function(responseBook) {
+//         console.log(responseBook);
+//     })
 
-            arrayMovies.push(popular);
-            arrayYear.push(date);
-            arrayOverview.push(overview);
-            arrayPoster.push(poster);
-            // ratings = JSON.stringify(movieRatings);
-            // plot = JSON.stringify(moviePlot);
-            
-            }
-            console.log(arrayMovies);
-
-            $("tbody").empty();
-            $(".content").append("<table>");
-            $("tbody").append("<th>"+"Title"+"</th");
-            $("tbody").append("<th>"+"Year"+"</th");
-            $("tbody").append("<th>"+"Plot"+"</th");
-            $("tbody").addClass("thead-dark");
-
-           for (i=0;i<arrayMovies.length;i++) {
-            $(".table-movie").append("<tr><td>"+arrayMovies[i]+ "<img src=" +arrayPoster[i]+" class="+"imgClass"+"></td><td>"+arrayYear[i]+"</td><td>"+arrayOverview[i]+"</td></tr>");
-           }
-
-        })
-        
-    })
-   
-        //TRENDING
-
-        $("#btnTrend").on("click", function(){
-            event.preventDefault();
-
-             var queryURLTrending = "https://api.themoviedb.org/3/trending/all/week?api_key=3ce487e5f4b9c4f747c250ad1fb0e3b0";
-
-            
-            
-
-        $.ajax ({
-            url: queryURLTrending,
-            method: 'GET'
-        }).then(function(response) {
-            console.log(response);
-
-
-            var arrayMovies = [];
-            var arrayOverview = [];
-            var arrayYear = [];
-            var arrayPoster = [];
-
-            for (let i=0;i<20;i++){
-            var moviePopular = response.results[i].original_title;
-            var movieOverview = response.results[i].overview;
-            var moviePoster = response.results[i].poster_path;
-            var movieDate = response.results[i].release_date;
-            
-            queryPoster = "http://image.tmdb.org/t/p/w185//"+moviePoster+"";
-            
-            // // let movieActors = response.Actors;
-            // // let movieRatings = response.Ratings[2];
-            // // let moviePlot = response.Plot;
-            popular = moviePopular;
-            overview = movieOverview;
-            poster = queryPoster;
-            date = movieDate;
-
-            if (typeof response.results[i].original_title != "undefined"){
-
-            arrayMovies.push(popular);
-            arrayYear.push(date);
-            arrayOverview.push(overview);
-            arrayPoster.push(poster);
-            
-            } 
-            
-            
-            }
-            console.log(arrayMovies);
-
-            $("tbody").empty();
-           $(".content").append("<table>");
-           $("tbody").append("<th>"+"Title"+"</th");
-            $("tbody").append("<th>"+"Year"+"</th");
-             $("tbody").append("<th>"+"Plot"+"</th");
-             $("tbody").addClass("thead-dark");
-             
-             
-
-           for (i=0;i<arrayMovies.length;i++) {
-            $(".table-movie").append("<tr><td>"+arrayMovies[i]+ "<img src=" +arrayPoster[i]+" class="+"imgClass"+"></td><td>"+arrayYear[i]+"</td><td>"+arrayOverview[i]+"</td></tr>");
-           }
-
-        })
-        
-        
-    })
-        
-
-        //by genre
-
-     $("#btnGenre").on("click", function(){
-            event.preventDefault();
-
-            var movie = $("#movieSearch :selected").val();
-            var genreSelected = $("#movieSearchGenre").val();
-            console.log(genreSelected);
-
-            var queryURLGenre = "https://api.themoviedb.org/3/discover/movie?api_key=3ce487e5f4b9c4f747c250ad1fb0e3b0&language=en-US&with_genres="+genreSelected+"&primary_release_year="+movie+"";
-
-        $.ajax ({
-            url: queryURLGenre,
-            method: 'GET'
-        }).then(function(response) {
-            
-
-            var arrayMovies = [];
-            var arrayOverview = [];
-            var arrayYear = [];
-            var arrayPoster = [];
-
-            for (let i=0;i<20;i++){
-            var moviePopular = response.results[i].original_title;
-            var movieOverview = response.results[i].overview;
-            var moviePoster = response.results[i].poster_path;
-            var movieDate = response.results[i].release_date;
-            
-            queryPoster = "http://image.tmdb.org/t/p/w185//"+moviePoster+"";
-            
-            // // let movieActors = response.Actors;
-            // // let movieRatings = response.Ratings[2];
-            // // let moviePlot = response.Plot;
-            popular = moviePopular;
-            overview = movieOverview;
-            poster = queryPoster;
-            date = movieDate;
-
-            if (typeof response.results[i].original_title != "undefined"){
-
-            arrayMovies.push(popular);
-            arrayYear.push(date);
-            arrayOverview.push(overview);
-            arrayPoster.push(poster);
-            
-            } 
-            
-            
-            }
-            console.log(arrayMovies);
-
-            $("tbody").empty();
-           $(".content").append("<table>");
-           $("tbody").append("<th>"+"Title"+"</th");
-            $("tbody").append("<th>"+"Year"+"</th");
-             $("tbody").append("<th>"+"Plot"+"</th");
-             $("tbody").addClass("thead-dark");
-             
-             
-
-           for (i=0;i<arrayMovies.length;i++) {
-            $(".table-movie").append("<tr><td>"+arrayMovies[i]+ "<img src=" +arrayPoster[i]+" class="+"imgClass"+"></td><td>"+arrayYear[i]+"</td><td>"+arrayOverview[i]+"</td></tr>");
-           }
-        })
-     })
+// })
